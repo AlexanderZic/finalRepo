632f067b848f3dde55bd17c69a3a9dd53176ce15
SS-93 <Sesughtor@gmail.com>
2023-10-30 19:44:03 -0700

    Logout with Clear Token

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,29 @@ import './App.css';
 import Navbar from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Signup from './Components/Auth/Signup/Signup';
-
+import {useEffect, useState} from 'react'
 function App() {
+const [token, setToken] = useState ('')
+
+useEffect (initializeToken, [])
+
+
+function initializeToken () {
+  setToken (localStorage.token)
+  console.log(localStorage.tokken)
+}
+
+function updateToken (newToken){
+setToken ('')
+localStorage.tokken = newToken}
+
+  function clearToken () {
+
+
+    setToken ('');
+    localStorage.removeItem.token;
+    
+  }
   return (
     <div className="App">
       <BrowserRouter>
